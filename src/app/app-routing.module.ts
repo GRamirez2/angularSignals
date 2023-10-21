d335a7476c6d3f0721d0352f413deef2bb1b5803
George Ramirez <george.ramirez2@gmail.com>
2023-10-21 17:44:56 -0500

    inital set up with dockerfile v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadComponent: () =>
+      import("./components/home/home.component").then((m) => m.HomeComponent),
+  },
+  {
+    path: "name",
+    loadComponent: () =>
+      import("./components/simple-name/simple-name.component").then(
+        (m) => m.SimpleNameComponent
+      ),
+  },
+  {
+    path: "about",
+    loadComponent: () =>
+      import("./components/about/about.component").then(
+        (m) => m.AboutComponent
+      ),
+  },
+  {
+    path: "**",
+    loadComponent: () =>
+      import("./components/dead-link/dead-link.component").then(
+        (m) => m.DeadLinkComponent
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
